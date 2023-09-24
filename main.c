3f14c5d3ab1f62cea05b6fea4b1c1e877672b0c7
Victor Fernandes <victorfariafernandes@gmail.com>
2023-09-24 13:26:38 -0300

    adding enc and dec methods to main

--- main.c
+++ main.c
@@ -1,38 +1,29 @@
 #include "gsw.c"
 #include <time.h>
 
-int main()
-{
-    srand(time(NULL));
-    int L = log2(q) + 1;
-    int N = K * L;
-    int m = 8;
 
-    int * t = GenerateVector(K);
-    int * secretKey = SecretKeyGen(t);
-    int * v = Powersof2(secretKey, L);
-
+int ** Encrypt(int message, int ** pubKey, int m, int N){
+    // BitDecomp (R * A)
     int ** R = GenerateBinaryMatrix(N,m);
-    
-    int ** A = PublicKeyGen(t, m); // pubK [m, K+1]
-
-    int ** RA = (MultiplyMatrixxMatrix(R, A, N, m, m, K+1));
-
+    int ** RA = (MultiplyMatrixxMatrix(R, pubKey, N, m, m, K+1));
     int ** r = applyRows(RA, N, K+1, &BitDecomp); // r [N, N]
 
+    // m * In
     int ** Identity = GenerateIdentity(N, N);
+    int ** mIdentity = MultiplyMatrixEscalar(message, Identity, N,N);
 
-    int ** mIdentity = MultiplyMatrixEscalar(1, Identity, N,N);
-
+    // m*In + BitDecomp(R * A)
     int ** sum = SumMatrixxMatrix(mIdentity, r, N,N);
 
+    // Flatten (m*In + BitDecomp(R * A))
     int ** C = applyRows(sum, N,N, &Flatten);
 
     printf("C \n");
+    int ** C_f = applyRows(sum, N,N, &BitDecompInverse);
     for(int i = 0; i < N; i++){
         printf("[%d][", i);
-        for(int j = 0; j < N; j++){
-            printf("%d ", C[i][j]);
+        for(int j = 0; j < K; j++){
+            printf("%d ", mod(C_f[i][j], q));
         }
         printf("]\n");
     }
@@ -55,23 +46,23 @@ int main()
         printf("]\n");
     }
 
-    printf("R \n");
-    for(int i = 0; i < N; i++){
-        printf("[%d][", i);
-        for(int j = 0; j < m; j++){
-            printf("%d ", R[i][j]);
-        }
-        printf("]\n");
-    }
+    // printf("R \n");
+    // for(int i = 0; i < N; i++){
+    //     printf("[%d][", i);
+    //     for(int j = 0; j < m; j++){
+    //         printf("%d ", R[i][j]);
+    //     }
+    //     printf("]\n");
+    // }
 
-    printf("A \n");
-    for(int i = 0; i < m; i++){
-        printf("[%d][", i);
-        for(int j = 0; j < K+1; j++){
-            printf("%d ", A[i][j]);
-        }
-        printf("]\n");
-    }
+    // printf("A \n");
+    // for(int i = 0; i < m; i++){
+    //     printf("[%d][", i);
+    //     for(int j = 0; j < K+1; j++){
+    //         printf("%d ", A[i][j]);
+    //     }
+    //     printf("]\n");
+    // }
 
     printf("RA \n");
     for(int i = 0; i < N; i++){
@@ -91,13 +82,39 @@ int main()
         printf("]\n");
     }
 
-    printf("message: %d \n", InternalProduct(C[L-1], v, L) / v[L-1]);
+    return C;
+}
+
+int Decrypt(int ** C, int * v, int L){
+    int ith = L-1; // L-1
+    int message =  mod(InternalProduct(C[ith], v, L), q )/ v[ith];
+    printf("message: %d \n", message);
+    printf("internal [%d]: %d  v[%d]: %d \n",ith,  mod(InternalProduct(C[ith], v, L), q ), ith, v[ith]);
+    return message;
+}
+
+int main()
+{
+    srand(time(NULL));
+    int L = log2(q) + 1;
+    int N = K * L;
+    int m = 8;
+
+    int * t = GenerateVector(K);
+    int * secretKey = SecretKeyGen(t);
+    int * v = Powersof2(secretKey, L);
+    int ** A = PublicKeyGen(t, m); // pubK [m, K+1]
+
+    int ** C = Encrypt(1, A, m, N);
+
+    int message = Decrypt(C, v, L);
 
     int res[L];
-    for(int j = L-1; j > 0; j--){
-        int ip = InternalProduct(C[j], v, L);
-        int dec = ip / v[j];
+    for(int j = L; j > 0; j--){
+        int ip = InternalProduct(C[j], v, j);
+        int dec = ip / v[j-1];
         res[j] = dec;
+        
         printf("%d ", dec);
     }
 
