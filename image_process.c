70fc8214d86e137bb570dc564bc403dab0d5d5e5
Victor Faria Fernandes <victorfariafernandes@gmail.com>
2024-03-06 02:45:04 -0300

    unified loop for image processing

--- image_process.c
+++ image_process.c
@@ -17,50 +17,38 @@ int main()
         int ** publicKey = PublicKeyGen(t, lwe); // pubK [m, n+1]
 
         byte *pixels_img_1;
-        int width_img_1;
-        int height_img_1;
-        int bytesPerPixel_img_1;
+        int width;
+        int height;
+        int bytesPerPixel;
         byte *pixels_img_2;
-        int width_img_2;
-        int height_img_2;
-        int bytesPerPixel_img_2;
-        byte *pixels_img_3 = (byte*)malloc(width_img_2*bytesPerPixel_img_2*height_img_2);
+        byte *pixels_img_3 = (byte*)malloc(width*bytesPerPixel*height);
 
 
-        ReadImage("resources/img1_10x10.bmp", &pixels_img_1, &width_img_1, &height_img_1,&bytesPerPixel_img_1);
-        ReadImage("resources/img2_10x10.bmp", &pixels_img_2, &width_img_2, &height_img_2,&bytesPerPixel_img_2);
-        printf("img1 size: %d %d total: %d ", width_img_1, height_img_1, width_img_1 * height_img_1);
-        printf("img2 size: %d %d total: %d ", width_img_2, height_img_2, width_img_2 * height_img_2);
+        ReadImage("resources/rectangle.bmp", &pixels_img_1, &width, &height,&bytesPerPixel);
+        ReadImage("resources/circle.bmp", &pixels_img_2, &width, &height,&bytesPerPixel);
+        printf("img1 size: %d %d total: %d ", width, height, width * height);
+        printf("img2 size: %d %d total: %d ", width, height, width * height);
 
-        cbyte * img_1_cPixels =  (cbyte *) malloc(sizeof(cbyte) * width_img_1 * height_img_1 * bytesPerPixel_img_1);
-        cbyte * img_2_cPixels =  (cbyte *) malloc(sizeof(cbyte) * width_img_2 * height_img_2 * bytesPerPixel_img_2);
-        cbyte * img_3_cPixels =  (cbyte *) malloc(sizeof(cbyte) * width_img_2 * height_img_2 * bytesPerPixel_img_2);
+        cbyte * img_1_cPixels =  (cbyte *) malloc(sizeof(cbyte) * width * height * bytesPerPixel);
+        cbyte * img_2_cPixels =  (cbyte *) malloc(sizeof(cbyte) * width * height * bytesPerPixel);
+        cbyte * img_3_cPixels =  (cbyte *) malloc(sizeof(cbyte) * width * height * bytesPerPixel);
 
-        for(int i = 0; i < width_img_1*bytesPerPixel_img_1; i++){
-                for(int j = 0; j < height_img_1; j++){
-                        img_1_cPixels[i * width_img_1 + j] = ByteEncrypt(pixels_img_1[i * width_img_1 + j], publicKey, lwe);
-                }
-                printf("finished %d row \n", i);
-        }
-        printf("first image");
-        for(int i = 0; i < width_img_2*bytesPerPixel_img_2; i++){
-                for(int j = 0; j < height_img_2; j++){
-                        img_2_cPixels[i * width_img_2 + j] = ByteEncrypt(pixels_img_2[i * width_img_2 + j], publicKey, lwe);
+        for(int i = 0; i < (int)(4 * ceil((float)width/4.0f))*bytesPerPixel; i++){
+                for(int j = 0; j < height; j++){
+                        img_1_cPixels[i * width + j] = ByteEncrypt(pixels_img_1[i * width + j], publicKey, lwe);
+                        img_2_cPixels[i * width + j] = ByteEncrypt(pixels_img_2[i * width + j], publicKey, lwe);
+                        img_3_cPixels[i * width + j] = ByteXOR(img_1_cPixels[i * width + j], img_2_cPixels[i * width + j],v,  lwe);
+                        pixels_img_3[i * width + j] = ByteDecrypt(img_2_cPixels[i * width + j], v, lwe);
                 }
                 printf("finished %d row \n", i);
         }
 
-        printf("second image");
-
-        for(int i = 0; i < width_img_2*bytesPerPixel_img_2; i++){
-                for(int j = 0; j < height_img_2; j++){
-                   img_3_cPixels[i * width_img_2 + j] = ByteXOR(img_1_cPixels[i * width_img_1 + j], img_2_cPixels[i * width_img_2 + j],v,  lwe);
-                   pixels_img_3[i * width_img_2 + j] = ByteDecrypt(img_3_cPixels[i * width_img_2 + j], v, lwe);
-                }
-                printf("finished %d row \n", i);
-        }
-        WriteImage("img3_10x10.bmp", pixels_img_3, width_img_2, height_img_2, bytesPerPixel_img_2);
-        // free(pixels_img_1);
-        // free(pixels_img_2);
+        WriteImage("img3_10x10.bmp", pixels_img_3, width, height, bytesPerPixel);
+        free(pixels_img_1);
+        free(pixels_img_2);
+        free(pixels_img_3);
+        free(img_1_cPixels);
+        free(img_2_cPixels);
+        free(img_3_cPixels);
         return 0;
 }
